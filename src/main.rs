cd8f4080738dfd2ec363ce34fd514704d286610a
ejaaskel <esa.jaaskela@suomi24.fi>
2022-03-09 20:32:37 +0200

    Initial commit, defining some variables

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+fn main() {
+    let opcode:u16;
+
+    let memory:[u8;4096];
+
+    let V:[u8;16];
+
+    let I:u16;
+    let pc:u16;
+
+    let gfx:[u8;64 * 32];
+
+    let delay_timer:u8;
+    let sound_timer:u8;
+
+    let stack:u16;
+    let sp:u16;
+
+    let key:[u8;16];
+}
